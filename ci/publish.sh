5f4d7e6fc0c3c705e53646d1bdb82ce0c4514b88
Ivan Gankevich <ivan@igankevich.com>
2024-12-08 14:14:49 +0100

    CI

--- ci/publish.sh
+++ ci/publish.sh
@@ -6,7 +6,7 @@ cargo_publish() {
     version="$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')"
     sed -i -e 's/^stuckliste =.*version.*$/stuckliste = { path = ".", version = "'"$version"'" }/' Cargo.toml
     for package in stuckliste stuckliste-cli; do
-        cargo publish --quiet --package "$package"
+        cargo publish --quiet --package "$package" --allow-dirty
     done
 }
 
