21398034e09e979ef33f90afce7a905570ee0b40
Ivan Gankevich <ivan@igankevich.com>
2024-12-08 14:04:53 +0100

    Enforce version.

--- ci/publish.sh
+++ ci/publish.sh
@@ -3,6 +3,8 @@
 . ./ci/preamble.sh
 
 cargo_publish() {
+    version="$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')"
+    sed -i -e 's/^stuckliste =.*version.*$/stuckliste = { path = ".", version = "'"$version"'" }' Cargo.toml
     for package in stuckliste stuckliste-cli; do
         cargo publish --quiet --package "$package"
     done
