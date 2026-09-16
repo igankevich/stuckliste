44cde8648ebe75659b90905ecc62a95db12f061b
Ivan Gankevich <ivan@igankevich.com>
2026-09-16 20:01:29 +0200

    Add build step.

--- ci/build.sh
+++ ci/build.sh
@@ -2,26 +2,38 @@
 
 . ./ci/preamble.sh
 
-if test "$GITHUB_ACTIONS" = "true" && test "$GITHUB_REF_TYPE" != "tag"; then
-    exit 0
-fi
+main() {
+    root="$(pwd)"
+    target=x86_64-unknown-linux-musl
+    rustup toolchain add "$RUST_VERSION" --target "$target" --profile minimal
+    cargo build \
+        --quiet \
+        --release \
+        --target "$target" \
+        --package stuckliste-cli
+    version="$GITHUB_REF_NAME"
+    rm -rf --one-file-system release
+    release_dir=release/"$version"
+    mkdir -p "$release_dir"/"$target"
+    for filename in lsbom mkbom; do
+        cp -vn target/"$target"/release/"$filename" "$release_dir"/"$target"/
+    done
+    cd "$release_dir"
+    create_tar_archive
+}
 
-set -e
-root="$(pwd)"
-target=x86_64-unknown-linux-musl
-rustup toolchain add "$RUST_VERSION" --target "$target" --profile minimal
-cargo build \
-    --quiet \
-    --release \
-    --target "$target" \
-    --package stuckliste-cli
-version="$GITHUB_REF_NAME"
-rm -rf --one-file-system release
-release_dir=release/"$version"
-mkdir -p "$release_dir"/"$target"
-for filename in lsbom mkbom; do
-    cp -vn target/"$target"/release/"$filename" "$release_dir"/"$target"/
-done
-cd "$release_dir"
-find . -type f -print0 >"$workdir"/files
-tar -czvf "$root"/stuckliste-"$version".tar.gz --null --files-from="$workdir"/files
+create_tar_archive() {
+    find . -type f -print0 | sort --unique --zero-terminated >"$workdir"/files
+    tar --create \
+        --mtime=@0 \
+        --numeric-owner \
+        --owner=0 \
+        --group=0 \
+        --gzip \
+        --verbose \
+        --file="$root"/stuckliste-"$version".tar.gz \
+        --null \
+        --files-from="$workdir"/files
+}
+
+main
